f9736db3f87437ffd1956cf8892accd40cd335e5
alyssoncrvg <alyssonpereira41@gmail.com>
2024-10-22 10:32:46 -0300

    update: routers delete

--- src/routes/DELETE/post.ts
+++ src/routes/DELETE/post.ts
@@ -1,21 +1,26 @@
 import { Router, Response } from "express";
-import { Posts } from "../../db/models";
+import { Posts, Usuario } from "../../db/models";
 import { authenticateToken } from "../../middlewares/authenticateToken";
 import { AuthenticatedRequest } from "../../../interfaces/authenticated";
 import { isVerify } from "../../middlewares/isVerify";
 
-export const postsRouterDelete = Router().delete('/post/:id', isVerify, authenticateToken, async (req: AuthenticatedRequest, res: Response) => {
+export const postsRouterDelete = Router().delete('/post/:id', authenticateToken, isVerify, async (req: AuthenticatedRequest, res: Response) => {
     const { id } = req.params;
 
     try {
 
         const postDelete = await Posts.findById(id);
 
-        console.log(postDelete?.user)
-        console.log(req.user?.id)
+        const userId = req.user?.id
         if (postDelete)
             if (postDelete.user == req.user?.id) {
-                postDelete.deleteOne()
+                await postDelete.deleteOne()
+
+                const userUpdate = await Usuario.findByIdAndUpdate(
+                    { userId },
+                    { $pull: { posts: id } }
+                )
+
                 res.status(201).json(postDelete)
             }
             else
--- src/routes/DELETE/user.ts
+++ src/routes/DELETE/user.ts
@@ -1,21 +1,44 @@
-import { Router, Request, Response } from "express";
-import { Usuario } from "../../db/models";
+import { Router, Response } from "express";
+import { Posts, Usuario } from "../../db/models";
 import { AuthenticatedRequest } from "../../../interfaces/authenticated";
 import { authenticateToken } from "../../middlewares/authenticateToken";
 import { isVerify } from "../../middlewares/isVerify";
 
-export const userRouterDelete = Router().delete('/user/:userName', isVerify, authenticateToken, async (req: AuthenticatedRequest, res: Response) => {
+export const userRouterDelete = Router().delete('/user/:userName', authenticateToken, isVerify, async (req: AuthenticatedRequest, res: Response) => {
     const id = req.user.id;
 
     try {
-        const userDelete = await Usuario.findByIdAndDelete(id)
 
-        if(userDelete)
-            res.status(201).json(userDelete)
-        else
-        res.status(404).json({
-            mensage: 'Usuário não encontrado'
-        })
+        await Posts.deleteMany({ user: id });
+
+        const updatedFollowers = await Usuario.updateMany(
+            { followers: id },
+            {
+                $pull: { followers: id }, 
+                $inc: { numberFollowers: -1 }
+            }
+        );
+
+        const updatedFollowing = await Usuario.updateMany(
+            { following: id },
+            {
+                $pull: { following: id },
+                $inc: { numberFollowing: -1 }
+            }
+        );
+
+        const userDelete = await Usuario.findByIdAndDelete(id);
+
+        if (userDelete) {
+            res.status(201).json({
+                message: 'Usuário deletado com sucesso',
+                user: userDelete
+            });
+        } else {
+            res.status(404).json({
+                message: 'Usuário não encontrado',
+            });
+        }
 
     } catch (error: any) {
 
